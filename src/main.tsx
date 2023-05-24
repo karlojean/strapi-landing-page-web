333325f3350b7cc42ee618d016e7007547ae9b45
karlojean <jeankarlodev@gmail.com>
2023-05-24 00:23:17 -0300

    config global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./templates/Home/index.tsx";
+import { GlobalStyles } from "./styles/global-styles.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>
 );
