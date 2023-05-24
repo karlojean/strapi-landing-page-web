5187fe8cde96e648570bd0c81da4631751e9187e
karlojean <jeankarlodev@gmail.com>
2023-05-24 00:54:16 -0300

    config jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testEnvironment: "jsdom",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/styles/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/**/stories.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/",
+  ],
+};
