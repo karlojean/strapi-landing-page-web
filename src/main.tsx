a96418e6f00a7b85c0ca1f501436ddc238fa2b29
karlojean <jeankarlodev@gmail.com>
2023-05-24 00:26:17 -0300

    config theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./templates/Home/index.tsx";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "./styles/theme.ts";
 import { GlobalStyles } from "./styles/global-styles.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>
 );
